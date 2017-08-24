0020f4490cec11735469bcdde41d76e771c5cbc2
SangPham <anhsang222@gmail.com>
2017-08-24 09:13:32 +0700

    initialize commit

---
+++ program.js
@@ -0,0 +1,10 @@
+var express = require('express')
+var path = require('path')
+var app = express()
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'templates'))
+app.use(express.static(process.argv[3] || path.join(__dirname, 'templates')))
+app.get('/home', function(req, res) {
+  res.render('index', {date: new Date().toDateString()})
+})
+app.listen(process.argv[2])
